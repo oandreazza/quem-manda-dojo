7a3572d3d41498aa83c4328c2ce105092e2b3d76
Daniel Wildt <dwildt@gmail.com>
2011-07-14 10:39:59 -0300

    dwildt: adicionando skeleton de teste

---
+++ test/cc/ttlabs/dojo/quemmanda/QuemMandaTest.java
@@ -0,0 +1,34 @@
+package cc.ttlabs.dojo.quemmanda;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class QuemMandaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	// Fulano é empregado de uma grande corporação
+	// Fulano quando está no trabalho não tem autoridade
+	// e quem manda por lá é o seu Chefe
+	
+	
+	// Fulano é lider de uma banda
+	// E quando está no palco é o dono da situação
+	// e junto com seus parceiros de banda fazem 
+	// a alegria do público
+	
+	
+	// Fulano tem uma familia, mulher e dois filhos
+	// mas em casa ele sempre tem autoridade
+	// de dizer a sua mulher quem é que manda: ela. 
+	
+	
+	
+}
